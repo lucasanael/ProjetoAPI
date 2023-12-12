8a34c29079e4f17c6824df7c7551648a8773e673
analobrigato <ana.lobrigato@gmail.com>
2023-12-12 11:29:56 -0300

    novo2

--- app/initial/page.jsx
+++ app/initial/page.jsx
@@ -18,8 +18,13 @@ function Initial() {
             <div className={Styles.texto}>
                 <p>Esse site foi produzido pelos alunos
                     Lucas Anael, Manuela Biacca, Ana Clara Lobrigato e Victoria Feitoza,
-                    utilizando a API da Marvel, que nos ofereceu acesso a um vasto universo de informações relacionadas aos icônicos personagens e histórias da Marvel Comics. Desenvolvida pela Marvel Entertainment, essa interface pública fornece acesso aos metadados dos seus quadrinhos, suportando autenticação, versionamento, recuperação de coleções de recursos ou recursos específicos pelo ID, cruzamento de origens de solicitações, compressões e tags utilizadas para evitar o retorno da mesma resposta mais de uma vez, se o conteúdo não mudou desde a primeira requisição.</p>
-                 Clique na foto do o personagem para descobrir sua história:
+                    utilizando a API da Marvel, que nos ofereceu acesso a um vasto universo de informações relacionadas aos icônicos personagens e histórias da Marvel Comics. Desenvolvida pela Marvel Entertainment, essa interface pública fornece acesso aos metadados dos seus quadrinhos, suportando autenticação, versionamento, recuperação de coleções de recursos ou recursos específicos pelo ID, cruzamento de origens de solicitações, compressões e tags utilizadas para evitar o retorno da mesma resposta mais de uma vez, se o conteúdo não mudou desde a primeira requisição.
+
+                </p>
+                <br />
+                Clique na foto do o personagem para descobrir sua história:
+
+
             </div>
 
             <div className={Styles.container}>
